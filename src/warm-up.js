f0642a27451d85b37f56ab462fb708240d0f94f9
Kayla Durrett <kayla.durrett@gmail.com>
2021-10-13 19:38:55 -0600

    Wrap up the warm up

--- src/warm-up.js
+++ src/warm-up.js
@@ -77,18 +77,20 @@ if(isFavorite === true){
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ['Name of the Wind', 'Enders Shadow', 'A Wise Mans Fear', 'Leviathan Wakes', 'Preacher'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+books[2];
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [7, 13, 20, 24, 30, 35, 42, 50, 75, 81, 90, 105];
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+belowTwoHundred[4];
 
 //------------------------- Objects -------------------------//
 
@@ -96,6 +98,12 @@ if(isFavorite === true){
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
 
+var dictionary = {
+  name: 'Webster\'s',
+  hasThesaurus: false,
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720;
