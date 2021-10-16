23fbf2b46e0f53040907a665930b7f1694c70d54
Kayla Durrett <kayla.durrett@gmail.com>
2021-10-15 19:35:13 -0600

    Inital Commit

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,12 @@
-
+function shelfBook(book, genre){
+  sciFiShelf = genre;
+  if (genre.length < 3) {
+    genre.unshift(book);
+  }
+  return (sciFiShelf, book);
+};
 module.exports = {
-  // shelfBook,
+   shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
